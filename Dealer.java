2cb528b936132fdfc5b95931ea92ba7430a5e57e
Scoot1116 <113002231+Scoot1116@users.noreply.github.com>
2023-02-01 23:24:52 -0800

    Completed

--- Dealer.java
+++ Dealer.java
@@ -1,10 +1,10 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*; 
 
 /**
- * Dealer.
+ * Dealer
  * 
- * @Scoot 
- * @1/26
+ * @SCOOT
+ * @2/1
  */
 
 import java.util.ArrayList;
@@ -15,9 +15,9 @@ public class Dealer extends Actor
      * Act - do whatever the Dealer wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    
     private int triplesRemaining;
     private Deck deck;
+    
     
     public void addedToWorld(World world)
     {
@@ -47,17 +47,67 @@ public class Dealer extends Actor
     
     public void checkIfEndGame()
     {
-        
+        if (triplesRemaining == 0)
+        {
+            Greenfoot.stop();
+        }
     }
     
-    public void checkIfTriple(ArrayList<Card> cardsOnBoard, Card[] cardsSelected,ArrayList<Integer> selectedCardsIndex)
+    public void checkIfTriple(ArrayList<Card> cardsOnBoard, Card[] cardsSelected, ArrayList<Integer> selectedCardsIndex)
     {
+        int shapes = cardsSelected[0].getShape().ordinal() + 
+                   cardsSelected[1].getShape().ordinal() +
+                   cardsSelected[2].getShape().ordinal();
+        int colors = cardsSelected[0].getColor().ordinal() + 
+                   cardsSelected[1].getColor().ordinal() +
+                   cardsSelected[2].getColor().ordinal();
+        int numOfShapes = cardsSelected[0].getNumberOfShapes() + 
+                   cardsSelected[1].getNumberOfShapes() +
+                   cardsSelected[2].getNumberOfShapes();
+        int shading = cardsSelected[0].getShading() + 
+                   cardsSelected[1].getShading() +
+                   cardsSelected[2].getShading();
         
+        if(shapes % 3 == 0 && colors % 3 == 0 && numOfShapes % 3 == 0 && shading % 3 == 0 )
+        {
+           removeAndReplaceTriple(cardsOnBoard, cardsSelected, selectedCardsIndex); 
+        }
+        else
+        {
+        
+        }
     }
     
-    private void removeAndReplaceTriple(ArrayList<Card> cardsOnBoard, Card[] cardsSelected,ArrayList<Integer> selectedCardsIndex)
+    private void removeAndReplaceTriple(ArrayList<Card> cardsOnBoard, Card[] cardsSelected, ArrayList<Integer> selectedCardsIndex)
     {
-        
+       // Set position of triple cards by coordinate, 3 cards by 2 coordinates 
+       int[][] cardsXYCoordinate = new int[3][2];  
+       for(int card = 0; card < 3; card++)
+       {
+            cardsXYCoordinate[card][0] = cardsSelected[card].getX();
+            cardsXYCoordinate[card][1] = cardsSelected[card].getY();
+       }
+       // Begin card animation off scene view     
+       Animations.slideAndTurn(cardsSelected);      
+
+       // Remove and replace triple cards
+        for(int card = 0; card < 3; card++)
+       { 
+           getWorld().removeObject(cardsSelected[card]);
+           if(deck.getNumCardsInDeck() > 0)
+           {
+               cardsOnBoard.set(selectedCardsIndex.get(card),deck.getTopCard());
+               getWorld().addObject(cardsOnBoard.get(selectedCardsIndex.get(card)), 
+                                                     cardsXYCoordinate[card][0], 
+                                                     cardsXYCoordinate[card][1]);
+           }
+       }
+       
+       // UI Housekeeping
+       triplesRemaining--;
+       Scorekeeper.updateScore();
+       setUI(); 
+       checkIfEndGame();
     }
     
     public Dealer(int numCardsInDeck)
---
+++ Player.java
@@ -0,0 +1,72 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * 
+ * 
+ * @Scoot
+ * @1/29
+ */
+
+import java.util.ArrayList;
+public class Player extends Actor
+{
+    /**
+     * Act - do whatever the Player wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    private Dealer dealer;
+    private Card[] cardsSelected;
+    private ArrayList<Card> cardsOnBoard;
+    private ArrayList<Integer> selectedCardsIndex;
+ 
+    
+    public void act()
+    {
+        selectCards();
+        dealer.checkIfTriple(cardsOnBoard, cardsSelected, selectedCardsIndex);
+        resetCardsSelected();
+    }
+    
+       public void addedToWorld(World world)
+    {
+        cardsOnBoard = (ArrayList) getWorld().getObjects(Card.class);
+    }
+    
+    private void selectCards()
+    {
+         for(int i = 0; i < cardsOnBoard.size(); i++)
+           {
+                  if(Greenfoot.mouseClicked(cardsOnBoard.get(i)))
+                 {
+                         if(cardsOnBoard.get(i).getIsSelected())
+                         {
+                              cardsOnBoard.get(i).setIsSelected(false);
+                              cardsOnBoard.get(i).setImage(cardsOnBoard.get(i).getCardImage());
+                              selectedCardsIndex.remove(new Integer(i));
+                         }
+                         else
+                         {
+                               cardsOnBoard.get(i).setIsSelected(true);
+                               cardsOnBoard.get(i).setImage(cardsOnBoard.get(i).getSelectedCardImage());
+                               selectedCardsIndex.add(new Integer(i));
+                         }
+                  }
+            }
+    }
+    
+    private void resetCardsSelected()
+    {
+         for(int i = 0; i < cardsOnBoard.size(); i++)
+          {
+                cardsOnBoard.get(i).setImage(cardsOnBoard.get(i).getCardImage());
+                cardsOnBoard.get(i).setIsSelected(false);
+          }
+          selectedCardsIndex.clear();
+    }
+    
+    private boolean threeCardsSelected()
+    {
+        return false;
+    }
+    
+}
